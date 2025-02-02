d6ba358bfcc8c16f03e8cef5dbdb638600d9fdf7
coelho566 <leandrofreestyle18@gmail.com>
2025-02-02 17:02:09 -0300

    Ajustes configurações sqs e mongodb

---
+++ src/main/java/com/framezip/management/application/config/aws/AmazonSqsConfig.java
@@ -0,0 +1,19 @@
+package com.framezip.management.application.config.aws;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import software.amazon.awssdk.auth.credentials.ProfileCredentialsProvider;
+import software.amazon.awssdk.regions.Region;
+import software.amazon.awssdk.services.sqs.SqsClient;
+
+@Configuration
+public class AmazonSqsConfig {
+
+    @Bean
+    public SqsClient sqsClient() {
+        return SqsClient.builder()
+                .region(Region.US_EAST_1)
+                .credentialsProvider(ProfileCredentialsProvider.create())
+                .build();
+    }
+}
