90660985675b2ba8eb7e1e73e0c7d4e2b175db72
coelho566 <leandrofreestyle18@gmail.com>
2025-02-09 14:55:38 -0300

    Ajustes configuração

---
+++ src/main/java/com/framezip/management/application/config/aws/AmazonConfig.java
@@ -0,0 +1,78 @@
+package com.framezip.management.application.config.aws;
+
+import io.awspring.cloud.sqs.operations.SqsTemplate;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import software.amazon.awssdk.auth.credentials.AwsSessionCredentials;
+import software.amazon.awssdk.auth.credentials.StaticCredentialsProvider;
+import software.amazon.awssdk.regions.Region;
+import software.amazon.awssdk.services.s3.S3Client;
+import software.amazon.awssdk.services.s3.presigner.S3Presigner;
+import software.amazon.awssdk.services.sqs.SqsAsyncClient;
+import software.amazon.awssdk.services.sqs.SqsClient;
+
+@Configuration
+public class AmazonConfig {
+
+
+    @Value("${spring.cloud.aws.credentials.access-key}")
+    private String accessKey;
+
+    @Value("${spring.cloud.aws.credentials.secret-key}")
+    private String secretKey;
+
+    @Value("${spring.cloud.aws.credentials.session-token}")
+    private String sessionToken;
+
+    @Value("${spring.cloud.aws.region.static}")
+    private String region;
+
+    @Bean
+    public AwsSessionCredentials awsSessionCredentials() {
+        return AwsSessionCredentials.create(accessKey, secretKey, sessionToken);
+    }
+
+    @Bean
+    SqsAsyncClient sqsAsyncClient(){
+        return SqsAsyncClient
+                .builder()
+                .region(Region.of(region))
+                .credentialsProvider(StaticCredentialsProvider
+                        .create(awsSessionCredentials()))
+                .build();
+        // add more Options
+    }
+
+    @Bean
+    public SqsClient sqsClient() {
+        return SqsClient.builder()
+                .region(Region.US_EAST_1)
+                .credentialsProvider(StaticCredentialsProvider
+                        .create(awsSessionCredentials()))
+                .build();
+    }
+
+    @Bean
+    public SqsTemplate sqsTemplate(SqsAsyncClient sqsAsyncClient){
+        return SqsTemplate.builder().sqsAsyncClient(sqsAsyncClient).build();
+    }
+
+    @Bean
+    public S3Client s3Client() {
+        return S3Client.builder()
+                .region(Region.US_EAST_1) // Região do S3
+                .credentialsProvider(StaticCredentialsProvider
+                        .create(awsSessionCredentials())) // Usa o perfil padrão
+                .build();
+    }
+
+    @Bean
+    public S3Presigner s3Presigner() {
+        return S3Presigner.builder()
+                .region(Region.US_EAST_1)
+                .credentialsProvider(StaticCredentialsProvider
+                        .create(awsSessionCredentials()))
+                .build();
+    }
+}
