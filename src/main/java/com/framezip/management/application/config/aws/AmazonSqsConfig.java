0a6cd51180162401586d5166b6b54f0625ad3819
coelho566 <leandrofreestyle18@gmail.com>
2025-02-09 13:56:36 -0300

    Ajustes configurações sqs

--- src/main/java/com/framezip/management/application/config/aws/AmazonSqsConfig.java
+++ src/main/java/com/framezip/management/application/config/aws/AmazonSqsConfig.java
@@ -1,7 +1,5 @@
 package com.framezip.management.application.config.aws;
 
-import io.awspring.cloud.sqs.listener.QueueNotFoundStrategy;
-import io.awspring.cloud.sqs.operations.SqsTemplate;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import software.amazon.awssdk.auth.credentials.ProfileCredentialsProvider;
@@ -27,14 +25,7 @@ public class AmazonSqsConfig {
         return SqsAsyncClient.builder()
                 .region(Region.US_EAST_1)
                 .credentialsProvider(ProfileCredentialsProvider.create())
-                .build();
-    }
-
-    @Bean
-    public SqsTemplate sqsTemplate() {
-        return SqsTemplate.builder()
-                .sqsAsyncClient(sqsAsyncClient())
-                .configure(o -> o.queueNotFoundStrategy(QueueNotFoundStrategy.FAIL))
+                .endpointOverride(URI.create("https://sqs.us-east-1.amazonaws.com/110832778598"))
                 .build();
     }
 }
