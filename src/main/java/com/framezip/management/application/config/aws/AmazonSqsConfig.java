2104c56ec33e1b999bfc75592c3c377df3f3c323
coelho566 <leandrofreestyle18@gmail.com>
2025-02-09 14:17:47 -0300

    Ajustes configurações sqs

--- src/main/java/com/framezip/management/application/config/aws/AmazonSqsConfig.java
+++ src/main/java/com/framezip/management/application/config/aws/AmazonSqsConfig.java
@@ -1,8 +1,13 @@
 package com.framezip.management.application.config.aws;
 
+import io.awspring.cloud.sqs.listener.QueueNotFoundStrategy;
+import io.awspring.cloud.sqs.operations.SqsTemplate;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import software.amazon.awssdk.auth.credentials.AwsBasicCredentials;
 import software.amazon.awssdk.auth.credentials.ProfileCredentialsProvider;
+import software.amazon.awssdk.auth.credentials.StaticCredentialsProvider;
 import software.amazon.awssdk.regions.Region;
 import software.amazon.awssdk.services.sqs.SqsAsyncClient;
 import software.amazon.awssdk.services.sqs.SqsClient;
@@ -11,6 +16,27 @@ import java.net.URI;
 
 @Configuration
 public class AmazonSqsConfig {
+
+
+    @Value("${spring.cloud.aws.credentials.access-key}")
+    private String accessKey;
+
+    @Value("${spring.cloud.aws.credentials.secret-key}")
+    private String secretKey;
+
+    @Value("${spring.cloud.aws.region.static}")
+    private String region;
+
+    @Bean
+    SqsAsyncClient sqsAsyncClient(){
+        return SqsAsyncClient
+                .builder()
+                .region(Region.of(region))
+                .credentialsProvider(StaticCredentialsProvider
+                        .create(AwsBasicCredentials.create(accessKey, secretKey)))
+                .build();
+        // add more Options
+    }
 
     @Bean
     public SqsClient sqsClient() {
@@ -21,11 +47,24 @@ public class AmazonSqsConfig {
     }
 
     @Bean
-    public SqsAsyncClient sqsAsyncClient() {
-        return SqsAsyncClient.builder()
-                .region(Region.US_EAST_1)
-                .credentialsProvider(ProfileCredentialsProvider.create())
-                .endpointOverride(URI.create("https://sqs.us-east-1.amazonaws.com/110832778598"))
-                .build();
+    public SqsTemplate sqsTemplate(SqsAsyncClient sqsAsyncClient){
+        return SqsTemplate.builder().sqsAsyncClient(sqsAsyncClient).build();
     }
+
+//    @Bean
+//    public SqsAsyncClient sqsAsyncClient() {
+//        return SqsAsyncClient.builder()
+//                .region(Region.US_EAST_1)
+//                .credentialsProvider(ProfileCredentialsProvider.create())
+//                .endpointOverride(URI.create("https://sqs.us-east-1.amazonaws.com/110832778598/filezip-send_email_queue"))
+//                .build();
+//    }
+
+//    @Bean
+//    public SqsTemplate sqsTemplate() {
+//        return SqsTemplate.builder()
+//                .sqsAsyncClient(sqsAsyncClient())
+//                .configure(o -> o.queueNotFoundStrategy(QueueNotFoundStrategy.FAIL))
+//                .build();
+//    }
 }
